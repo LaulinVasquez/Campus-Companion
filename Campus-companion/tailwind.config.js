4d7563d338fa35f62082d3e51a1f7e5cf504752c
Laurin Vasquez <113741390+LaulinVasquez@users.noreply.github.com>
2025-12-21 19:49:04 -0700

    adding dependencies

---
+++ Campus-companion/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./App.{js,jx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
